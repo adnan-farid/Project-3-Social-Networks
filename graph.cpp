bdae0289af5999fc1ec699feda9db5f61151d0d9
adnan-farid <faridadnan810@gmail.com>
2023-12-03 16:58:10 -0500

    added weights to edges and dijkstras alg implementation.

--- graph.cpp
+++ graph.cpp
@@ -7,37 +7,100 @@
 #include <vector>
 #include <fstream>
 #include <sstream>
+#include <queue>
+#include <limits>
+#include <algorithm>
+#include <set>
+#include <chrono>
 using namespace std;
 class Graph{
 private:
-    unordered_map<int, vector<int>> adjacencyList;
+    unordered_map<int, vector<pair<int,int>>> adjacencyList;
+    int nodeAmt;
+
+    void setNodeAmt(int nodeAmt) {
+        this->nodeAmt = nodeAmt;
+    }
 public:
-    void addEdge(int from, int to) {
+    Graph() {
+        nodeAmt = 0;
+    }
+    void addEdge(int from, int to, int weight = 1) {
         //(from -> to)
-        adjacencyList[from].push_back(to);
+        adjacencyList[from].push_back(make_pair(to, weight));
         //add another edge (to -> from) because the graph is undirected
-        adjacencyList[to].push_back(from);
+        adjacencyList[to].push_back(make_pair(from, weight));
     }
     void readDataset(const string &filename) {
         ifstream file(filename);
         string line;
         //get rid of first line in csv
         getline(file, line);
+        set<int> nodeCounter;
         while (getline(file, line)) {
             stringstream ss(line);
             string from, to;
             if(getline(ss, from, ',') && getline(ss,to)) {
                 addEdge(stoi(from), stoi(to));
+                nodeCounter.insert(stoi(from));
+                nodeCounter.insert(stoi(to));
             }
         }
+        setNodeAmt(nodeCounter.size());
+    }
+    void getGraphSize() {
+        cout << "There are " << nodeAmt << " Nodes." << endl;
     }
     void printGraph() {
         for (auto& pair : adjacencyList) {
             cout << "Node " << pair.first << " connects to: ";
-            for (int node : pair.second) {
-                cout << node << " ";
+            for (auto& edge : pair.second) {
+                cout << edge.first << " ";
             }
             cout << endl;
         }
+    }
+    pair<int,vector<int>> dijkstra(int start, int end, double &executionTime) {
+        using namespace std::chrono;
+        auto startTime = high_resolution_clock::now();        unordered_map<int, int> distances;
+        unordered_map<int, int> before;
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        for (auto& pair: adjacencyList) {
+            distances[pair.first] = numeric_limits<int>::max();
+        }
+        distances[start] = 0;
+        pq.push(make_pair(0,start));
+
+        while(!pq.empty()) {
+            int curr = pq.top().second;
+            pq.pop();
+            if (curr == end) {
+                break;
+            }
+
+            for (auto& adjacentNode : adjacencyList[curr]) {
+                int next = adjacentNode.first;
+                int weight = adjacentNode.second;
+                int currDist = distances[curr] + weight;
+
+                if (currDist < distances[next]) {
+                    distances[next] = currDist;
+                    before[next] = curr;
+                    pq.push(make_pair(currDist, next));
+                }
+            }
+
+        }
+        vector <int> dijkstraPath;
+        if (distances[end] != numeric_limits<int>::max()) {
+            for (int i = end; i != start; i = before[i]) {
+                dijkstraPath.push_back(i);
+            }
+            dijkstraPath.push_back(start);
+            reverse(dijkstraPath.begin(), dijkstraPath.end());
+        }
+        auto endTime = high_resolution_clock::now();
+        executionTime = duration<double>(endTime - startTime).count();
+        return make_pair(distances[end], dijkstraPath);
     }
 };
--- main.cpp
+++ main.cpp
@@ -3,6 +3,17 @@
 int main() {
     Graph graph;
     graph.readDataset("facebook_clean_data\\artist_edges.csv");
-    graph.printGraph();
+    graph.getGraphSize();
+    double executionTime;
+    auto result = graph.dijkstra(1324, 50325, executionTime);
+    cout << "Execution time: " << executionTime << " seconds" << endl;
+    int shortestDistance = result.first;
+    vector<int> shortestPath = result.second;
+
+    cout << "Shortest distance: " << shortestDistance << endl;
+    cout << "Shortest path: ";
+    for (int node : shortestPath) {
+        cout << node << " ";
+    }
     return 0;
 }
